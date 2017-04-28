4286b562764771672e95a28f4629074b8e37b02e
Adhithya <adhithyaprabhu77@gmail.com>
2017-04-28 23:31:27 +0530

    Search function timestamp

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,8 +1,18 @@
 var React = require('react');
 
+var moment = require('moment');
 var Todo = React.createClass ({
 		render: function () {
-			var {id,text, completed} = this.props;
+			var {id,text, completed, createdAt, completedAt} = this.props;
+			var renderDate = () => {
+				var message = 'created';
+				var timestamp = createdAt;
+				if(completed) {
+					message = 'completed';
+					timestamp = completedAt;
+				}
+				return message + moment.unix(timestamp).format('MMMM Do YYYY @ h:mm a');
+			}
 			return (
 				<div onClick={() => {
 					this.props.onToggle(id);
@@ -10,7 +20,8 @@ var Todo = React.createClass ({
 					<input id="checkbox1" type="checkbox"
 					checked={completed} />
 					<label for="checkbox1">Show  Completed</label>
-					{text}
+					<p>{text}</p>
+					<p>{renderDate()}</p>
 				</div>
 				);
 		}
